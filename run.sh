91bd58b7a7b6988ffa463a5c674934714c817a7e
ywshin <shinforever12@gmail.com>
2026-06-29 04:05:23 +0900

    updates2

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 export PYTHONPATH="$PWD/src"
-python -m autosub --source auto --language en --model base.en --show-levels "$@"
+python -m autosub --source auto --language en --model base.en "$@"
